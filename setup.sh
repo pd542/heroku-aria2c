84b97faf52449cae7206a44beb3fe3f15c7b4e0b
pd542 <pengdl3@mail2.sysu.edu.cn>
2020-09-19 14:44:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,5 +18,5 @@ wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
 wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 
 # Tracker
-tracker_list=`curl -Ns https://cdn.jsdelivr.net/gh/ngosang/trackerslist/trackers_all.txt | awk '$1' | tr '\n' ',' | cat`
+tracker_list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 echo "bt-tracker=$tracker_list" >> aria2c.conf
